ef29bb18c1e5a8d39a1bcf435acbda0495265829
MateuszCiszkiewicz <aamadeusz94@gmail.com>
2020-03-29 18:02:09 +0200

    Training: for instance loop v2 with secured amount

--- mLotto.py
+++ mLotto.py
@@ -1,23 +1,28 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 
 import random
 
-liczba = random.randint(1, 10)
+number = random.randint(1, 10)
 
-for i in range (3):
-    print ("Proba", i+1 )
-    odp = input("Jaką liczbę od 1 do 10 masz na myśli ? ")
-
-    if liczba == int(odp) :
-
-        print("podałeś:", odp)
-        print("wylosowana", liczba)
-        print("zgadłeś")
-        break
+for i in range(3):
+    print()
+    print("Proba", i + 1)
+    answer = int (input("Jaką liczbę od 1 do 10 masz na myśli ? "))
+    if answer < 0 or answer > 10:
+        print("zakres")
     else:
-        print("podałeś:", odp)
-        print("niezgadłeś")
+        if number == int(answer):
 
-
+            print("podałeś:", answer)
+            print("wylosowana", number)
+            print("zgadłeś")
+            break
+        elif i == 2:
+            print()
+            print("podałeś:", answer)
+            print("Miałem na myśli liczbe", number)
+        else:
+            print("podałeś:", answer)
+            print("niezgadłeś, sprobuj ponownie")
