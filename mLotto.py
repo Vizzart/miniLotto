16e53f4ba2d65a606ef7f3933ab765fff7f991ab
MateuszCiszkiewicz <aamadeusz94@gmail.com>
2020-03-29 16:46:55 +0200

    Training: for instance loop

--- mLotto.py
+++ mLotto.py
@@ -6,16 +6,18 @@ import random
 
 liczba = random.randint(1, 10)
 
-odp = input ("Jaką liczbę od 1 do 10 masz na myśli ? ")
 
-print("podałeś:", odp)
+for i in range (10):
+    odp = input("Jaką liczbę od 1 do 10 masz na myśli ? ")
 
-print ("wylosowana", liczba)
-
-if liczba == int(odp) :
-    print("zgadłeś")
-else:
-    print("niezgadłeś")
-
+    if liczba == int(odp) :
+        print("podałeś:", odp)
+        print("wylosowana", liczba)
+        print("zgadłeś")
+        break
+    else:
+        print("podałeś:", odp)
+     #  print("wylosowana", liczba)
+        print("niezgadłeś")
 
 
