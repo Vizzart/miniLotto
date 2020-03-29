ac12f35dbbb357e71c7a53f296ab65cd6bee9e91
MateuszCiszkiewicz <aamadeusz94@gmail.com>
2020-03-29 16:33:58 +0200

    Trening: Simple if/else instance.

--- mLotto.py
+++ mLotto.py
@@ -1,2 +1,21 @@
 #/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+import random
+
+liczba = random.randint(1, 10)
+
+odp = input ("Jaką liczbę od 1 do 10 masz na myśli ? ")
+
+print("podałeś:", odp)
+
+print ("wylosowana", liczba)
+
+if liczba == int(odp) :
+    print("zgadłeś")
+else:
+    print("niezgadłeś")
+
+
+
